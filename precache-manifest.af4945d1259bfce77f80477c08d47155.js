8888f9dfcaff544ff6132172c3904aa4dcf27d8a
Vladyslav Chychykalo <vladyslav.chychykalo@gmail.com>
2019-12-08 20:26:33 +0200

    Updates

---
+++ precache-manifest.af4945d1259bfce77f80477c08d47155.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae571d9c11da8a31bcab057c2eebffb",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "4c24053529cf369e4be3",
+    "url": "/goit-react-hw-04-movies/static/js/2.908ad0f4.chunk.js"
+  },
+  {
+    "revision": "7709af4467eb10559879",
+    "url": "/goit-react-hw-04-movies/static/js/main.e6ff5cc5.chunk.js"
+  },
+  {
+    "revision": "8d2254b44b6d7071638a",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.0824e930.js"
+  }
+]);
